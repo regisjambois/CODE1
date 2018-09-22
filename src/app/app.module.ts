2842181440718aff1e647dcb003ad10f498f34b7
regisjambois <33594622+regisjambois@users.noreply.github.com>
2018-09-22 11:16:04 +0200

    INIT

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
